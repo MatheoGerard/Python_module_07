b3d6cc019e5df6caab352bcdb3de81a1421fc41f
mgerard <mgerard@student.42belgium.be>
2026-06-19 22:39:10 +0200

    Add factorie class
    Add factory abstract class to instantiate creature directly with a
    method.

--- ex0/battle.py
+++ ex0/battle.py
@@ -2,13 +2,37 @@ from abc import ABC, abstractmethod
 
 
 class Creature(ABC):
-    def __init__(self) -> None:
-        self._name: str = ""
-        self._type: str = ""
+    def __init__(self, name: str, crea_type: str) -> None:
+        self._name: str = name
+        self._crea_type: str = crea_type
 
     @abstractmethod
-    def attack() -> str:
+    def attack(self) -> str:
         pass
 
     def describe(self) -> str:
-        return f"{self._name} is a {self._type} type Creature"
+        return f"{self._name} is a {self._crea_type} type Creature"
+
+
+class CreatureFactory(ABC):
+    @abstractmethod
+    def create_base() -> "Creature":
+        pass
+
+    @abstractmethod
+    def create_evolved() -> "Creature":
+        pass
+
+
+class Flameling(Creature):
+    def __init__(self, name: str, crea_type: str) -> None:
+        super().__init__(name, crea_type)
+
+    def attack(self) -> str:
+        return f"{self._name} uses Ember!"
+
+
+if __name__ == "__main__":
+    print("Testing factory")
+    pokemon: Flameling = Flameling("Flameling", "Fire")
+    print(pokemon.describe())
